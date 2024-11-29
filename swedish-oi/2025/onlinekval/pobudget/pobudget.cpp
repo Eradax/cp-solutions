729dadcbb0ed4828558f64b9eda6db69d9f99a0e
Eradax <erikadebahr@gmail.com>
2024-11-29 14:26:15 +0100

    Solved PO onlinekval 2025 pobudget

---
+++ swedish-oi/2025/onlinekval/pobudget/pobudget.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i< (n); i++)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m = 0;
+
+    int n;
+    cin >> n;
+
+    rep(i, n) {
+        string l;
+        int c;
+        cin >> l >> c;
+        m += c;
+    }
+
+    if (m == 0) {
+        cout << "Lagom";
+    } else if (m > 0) {
+        cout << "Usch, vinst";
+    } else {
+        cout << "Nekad";
+    }
+
+    cout << endl;
+}
