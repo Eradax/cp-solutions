a1a1f62101a56c7198ac1b66b99f06f59983aa44
Eradax <erikadebahr@gmail.com>
2025-01-08 21:23:55 +0100

    move icpc in to it's own folder, solve integerdivision

---
+++ icpc/rocky-mountain-rc/2019/integerdivision/integerdivision.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#ifdef DBG
+#include "../../../../dbg.h"
+#else
+#define dbg(...)
+#endif
+
+/*
+source:
+    Rocky Mountain Regional Programming Contest 2019
+links:
+    https://open.kattis.com/problems/integerdivision
+*/
+
+using namespace std;
+
+using ull = unsigned long long;
+
+using vi = vector<int>;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+  int n, d;
+  cin >> n >> d;
+
+  unordered_map<int, int> m;
+  rep(i, n) {
+    int tmp;
+    cin >> tmp;
+  
+    m[tmp / d]++;
+  }
+
+  ull ans = 0;
+  for (auto& e : m) {
+    ans += ull(e.second) * (ull)(e.second - 1) / 2;
+  }
+
+  cout << ans << endl;
+}
