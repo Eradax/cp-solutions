46b145c268b835f78b36d0bca9fbdc505e6083cb
Eradax <erikadebahr@gmail.com>
2025-03-18 22:11:55 +0100

    better gen on diplomacy (NOI 2025)

--- nordic-oi/2025/diplomacy/gen.py
+++ nordic-oi/2025/diplomacy/gen.py
@@ -12,36 +12,33 @@ q = []
 
 cnt = 0
 while cnt < Q:
-    op = randint(0, 2)
+    match randint(0, 2):
+        case 0:
+            edge = tuple(sorted((randint(1, N), randint(1, N))))
+            if edge in edge_set or edge[0] == edge[1]:
+                continue
 
-    if op == 0:
-        edge = tuple(sorted((randint(1, N), randint(1, N))))
-        if edge in edge_set or edge[0] == edge[1]:
-            continue
-
-        i = randint(1, N**2)
-        if i in used_p:
             i = randint(1, N**2)
+            if i in used_p:
+                i = randint(1, N**2)
 
-        heappush(edges, (-i, edge))
-        edge_set.add(edge)
-        used_p.add(i)
+            heappush(edges, (-i, edge))
+            edge_set.add(edge)
+            used_p.add(i)
 
-        q.append(('a', edge[0], edge[1], i))
-    elif op == 1:
-        if len(edges) == 0:
-            continue
-        q.append(('r'))
-        i, edge = heappop(edges)
-        i = -i
-        used_p.remove(i)
-        edge_set.remove(edge)
-    else:
-        q.append(('d'))
+            q.append(("a", edge[0], edge[1], i))
+        case 1:
+            if len(edges) == 0:
+                continue
+            q.append("r")
+            i, edge = heappop(edges)
+            used_p.remove(-i)
+            edge_set.remove(edge)
+        case 2:
+            q.append("d")
 
     cnt += 1
 
 print(N, Q)
 for i in q:
     print(*i)
-
